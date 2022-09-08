4077bf5a4dbfa1c1da9a98cd81874da18e754aa1
Brian Liao <yaramiu@gmail.com>
2022-09-08 14:19:23 -0400

    Answer repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let cumulativeString = "";
 
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    cumulativeString += string;
+  }
+
+  return cumulativeString;
 };
 
 // Do not edit below this line
