e1df05c9d504ea05773cb0cba149b9fbe4504cf3
Brian Liao <yaramiu@gmail.com>
2022-09-08 14:39:39 -0400

    Answer reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  const characterArray = string.split("");
+  let reversedString = "";
 
+  const reversedCharacterArray = characterArray.reverse();
+  reversedString = reversedCharacterArray.join("");
+
+  return reversedString;
 };
 
 // Do not edit below this line
