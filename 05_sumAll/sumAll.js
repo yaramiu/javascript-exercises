a26f34c44d9c1623514e13101ac2b7404b2c3fcb
Brian Liao <yaramiu@gmail.com>
2022-09-08 16:51:25 -0400

    Answer sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function (startNumber, endNumber) {
+  if (!isValidNumber(startNumber) || !isValidNumber(endNumber)) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+
+  if (startNumber <= endNumber) {
+    for (let i = startNumber; i <= endNumber; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = startNumber; i >= endNumber; i--) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
+
+function isValidNumber(number) {
+  if (number < 0 || !(typeof number === "number")) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
