555c621b7ccfba04be63f6a18b3df904e0c99c09
Brian Liao <yaramiu@gmail.com>
2022-09-08 17:23:19 -0400

    Answer tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (degreesFahrenheit) {
+  return Number(((degreesFahrenheit - 32) / 1.8).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (degreesCelsius) {
+  return Number((degreesCelsius * 1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
