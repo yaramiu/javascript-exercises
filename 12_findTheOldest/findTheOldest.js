d49a8ff231cbef5db7ecda3231b06a5c0dd34416
Brian Liao <yaramiu@gmail.com>
2022-09-13 18:32:42 -0400

    Answer findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+  let oldestAge = 0;
+  let oldestPerson;
 
+  peopleArray.forEach((person) => {
+    if ("yearOfDeath" in person) {
+      age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      const currentYear = new Date().getFullYear();
+      age = currentYear - person.yearOfBirth;
+    }
+    person["age"] = age;
+
+    if (age >= oldestAge) {
+      oldestAge = age;
+      oldestPerson = person;
+    }
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
