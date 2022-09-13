958c771611bbbc6fd7b4c004e1d09f6bbffd4eb4
Brian Liao <yaramiu@gmail.com>
2022-09-13 17:40:30 -0400

    Answer fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (number < 0) {
+    return "OOPS";
+  } else if (number == 0) {
+    return 0;
+  }
 
+  let fibonacciNumbersArray = [];
+
+  if (number == 1 || number == 2) {
+    return 1;
+  }
+
+  fibonacciNumbersArray.push(1);
+  fibonacciNumbersArray.push(1);
+
+  for (let i = 2; i < number; i++) {
+    let currentFibonacciNumber =
+      fibonacciNumbersArray[i - 1] + fibonacciNumbersArray[i - 2];
+    fibonacciNumbersArray.push(currentFibonacciNumber);
+  }
+
+  return fibonacciNumbersArray[fibonacciNumbersArray.length - 1];
 };
 
 // Do not edit below this line
