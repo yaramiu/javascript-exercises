177e9a42794f1ae611d0c9bf74aa3e6703b0f01f
Brian Liao <yaramiu@gmail.com>
2022-09-13 17:13:08 -0400

    Answer palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase();
+  string = string.replace(/\W|_/g, "");
+  let charactersArray = string.split("");
 
+  let reversedCharactersArray = [];
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedCharactersArray.push(string[i]);
+  }
+
+  if (charactersArray.join() === reversedCharactersArray.join()) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
